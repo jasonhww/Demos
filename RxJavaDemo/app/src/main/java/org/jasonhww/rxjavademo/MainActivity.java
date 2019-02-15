245ca27f38ac79949d29a32b2d83234da504aa52
jasonhww <jasonhww@163.com>
2019-02-15 15:36:14 +0800

    添加rXJAVA

--- RxJavaDemo/app/src/main/java/org/jasonhww/rxjavademo/MainActivity.java
+++ RxJavaDemo/app/src/main/java/org/jasonhww/rxjavademo/MainActivity.java
@@ -1,13 +1,262 @@
 package org.jasonhww.rxjavademo;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import io.reactivex.*;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        baseUse();
+//        baseUseFlatMap();
+//        baseUseFlowable();
+//        baseUseFlowable2();
+//        test();
     }
+
+
+    /**
+     * 基本使用
+     */
+    public void baseUse() {
+
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                Log.d(TAG, "emit 1");
+                e.onNext(1);
+                Log.d(TAG, "emit 2");
+                e.onNext(2);
+                Log.d(TAG, "emit 3");
+                e.onNext(3);
+                Log.d(TAG, "emit onComplete");
+                e.onComplete();
+                Log.d(TAG, "emit 4");
+                e.onNext(4);
+
+            }
+        }).subscribe(new Observer<Integer>() {
+            private Disposable mDisposable;
+            private int i;
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe:d " + d);
+                mDisposable = d;
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                if (i == 2 && mDisposable != null) {
+                    mDisposable.dispose();
+                    Log.d(TAG, "mDisposable.isDisposed()=" + mDisposable.isDisposed());
+                }
+                Log.d(TAG, "onNext: " + integer);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError: ");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: ");
+            }
+        });
+
+    }
+
+    @SuppressLint("CheckResult")
+    public void baseUseFlatMap() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                Log.d(TAG, "emit 1");
+                e.onNext(1);
+                Log.d(TAG, "emit 2");
+                e.onNext(2);
+                Log.d(TAG, "emit 3");
+                e.onNext(3);
+                Log.d(TAG, "emit onComplete");
+                e.onComplete();
+            }
+        }).flatMap(new Function<Integer, ObservableSource<String>>() {
+                    @Override
+                    public ObservableSource<String> apply(Integer integer) throws Exception {
+                        List<String> lists = new ArrayList<>();
+                        for (int i = 0; i < 3; i++) {
+                            lists.add(i + "-->>i am result" + integer);
+                        }
+                        return Observable.fromIterable(lists).delay(10, TimeUnit.MILLISECONDS);
+                    }
+                }).subscribe(new Consumer<String>() {
+
+            @Override
+            public void accept(String str) throws Exception {
+                Log.d(TAG, "accept:Str " + str);
+            }
+
+        }, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                Log.d(TAG, "Throwable: " + throwable.getMessage());
+            }
+        });
+    }
+
+    public void baseUseFlowable() {
+        Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
+                Log.d(TAG, "emit 1");
+                emitter.onNext(1);
+                Log.d(TAG, "after emit 1 requested:" + emitter.requested());
+                Log.d(TAG, "emit 2");
+                emitter.onNext(2);
+                Log.d(TAG, "after emit 2 requested:" + emitter.requested());
+                Log.d(TAG, "emit 3");
+                emitter.onNext(3);
+                Log.d(TAG, "after emit 3 requested:" + emitter.requested());
+                Log.d(TAG, "emit complete");
+                emitter.onComplete();
+            }
+        }, BackpressureStrategy.ERROR).subscribe(new Subscriber<Integer>() {
+
+            @Override
+            public void onSubscribe(Subscription s) {
+                Log.d(TAG, "onSubscribe");
+                s.request(100);//处理事件的能力
+                s.request(10);//处理事件的能力
+                //总的能力值为110
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "onNext: " + integer);
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                Log.w(TAG, "onError: ", t);
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete");
+            }
+        });
+    }
+
+    private Subscription mSubscription;
+
+    public void baseUseFlowable2() {
+        Flowable
+                .create(new FlowableOnSubscribe<Integer>() {
+                    @Override
+                    public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
+                        Log.d(TAG, "First requested = " + emitter.requested());
+                        boolean flag;
+                        for (int i = 0; ; i++) {
+                            flag = false;
+                            while (emitter.requested() == 0) {
+                                if (!flag) {
+                                    Log.d(TAG, "Oh no! I can't emit value!");
+                                    flag = true;
+                                }
+                            }
+                            emitter.onNext(i);
+                            Log.d(TAG, "emit " + i + " , requested = " + emitter.requested());
+                        }
+                    }
+                }, BackpressureStrategy.ERROR)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Integer>() {
+
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        Log.d(TAG, "onSubscribe");
+                        mSubscription = s;
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.d(TAG, "onNext: " + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        Log.w(TAG, "onError: ", t);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "onComplete");
+                    }
+                });
+
+    }
+
+
+    public void test() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+
+            }
+        }).subscribeOn(Schedulers.io()).map(new Function<Integer, String>() {
+            @Override
+            public String apply(Integer integer) throws Exception {
+                return "i am map change" + integer;
+            }
+        }).flatMap(new Function<String, ObservableSource<String>>() {
+            @Override
+            public ObservableSource<String> apply(String s) throws Exception {
+                return null;
+            }
+        }).subscribeOn(Schedulers.newThread()).subscribeOn(Schedulers.computation())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(String str) {
+                        Log.d(TAG, "onNext: " + str);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
 }
